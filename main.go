46ff674c404d1bd3ddcae7a18c6a092d80b47b7e
Janitha Jayaweera <janitha_j@hotmail.com>
2021-09-02 16:20:51 -0700

    ~/go/src/sigs.k8s.io/kubebuilder/bin/kubebuilder create api --group wasmcloud.com --version v1alpha1 --kind App
    Signed-off-by: Janitha Jayaweera <janitha_j@hotmail.com>

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	wasmcloudcomv1alpha1 "wasmcloud-k8s-operator/app/api/v1alpha1"
+	"wasmcloud-k8s-operator/app/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(wasmcloudcomv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.AppReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "App")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
