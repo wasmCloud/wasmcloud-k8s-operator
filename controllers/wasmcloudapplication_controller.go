9cb34055a84bfeda1be6c06469fda3a3c2ab1e2f
David Laban <david.laban@red-badger.com>
2021-09-16 11:57:46 +0100

    boilerplate from https://book.kubebuilder.io/reference/using-finalizers.html
    Signed-off-by: David Laban <david.laban@red-badger.com>

--- controllers/wasmcloudapplication_controller.go
+++ controllers/wasmcloudapplication_controller.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"github.com/go-logr/logr"
 	corev1beta1 "github.com/wasmCloud/wasmcloud-k8s-operator/api/v1beta1"
@@ -40,19 +41,41 @@ type WasmCloudApplicationReconciler struct {
 func (r *WasmCloudApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := r.Log.WithValues("wasmcloud-lattice-controller", req.NamespacedName)
 
-	var app corev1beta1.WasmCloudApplication
+	var app *corev1beta1.WasmCloudApplication
 	log.Info("reconciling the requested manifest", "request", req)
 
-	if err := r.Get(ctx, req.NamespacedName, &app); err != nil {
-		(&request.Sender{
-			Log: log,
-		}).Send(request.Message{
-			Name:        req.Name,
-			Namespace:   req.Namespace,
-			Application: nil,
-		})
+	if err := r.Get(ctx, req.NamespacedName, app); err != nil {
+		log.Error(err, "unable to fetch app")
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-		return ctrl.Result{}, nil
+	appFinalizerName := "core.wasmcloud.com/finalizer"
+
+	if app.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !containsString(app.GetFinalizers(), appFinalizerName) {
+			controllerutil.AddFinalizer(app, appFinalizerName)
+
+			if err := r.Update(ctx, app); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		if containsString(app.GetFinalizers(), appFinalizerName) {
+			if err := r.deleteExternalResources(app); err != nil {
+				// if fail to delete the external dependency here, return with error
+				// so that it can be retried
+				return ctrl.Result{}, err
+			}
+
+			// remove our finalizer from the list and update it.
+			controllerutil.RemoveFinalizer(app, appFinalizerName)
+			if err := r.Update(ctx, app); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
 	}
 
 	response, err := (&request.Sender{
@@ -71,7 +94,7 @@ func (r *WasmCloudApplicationReconciler) Reconcile(ctx context.Context, req ctrl
 
 	log.Info("response from the lattice controller", "application", response.Status)
 
-	if err := r.Status().Update(context.Background(), &app); err != nil {
+	if err := r.Status().Update(context.Background(), app); err != nil {
 		log.Info("error updating the status", "error", err)
 		return ctrl.Result{}, err
 	}
@@ -85,4 +108,36 @@ func (r *WasmCloudApplicationReconciler) SetupWithManager(mgr ctrl.Manager) erro
 		For(&corev1beta1.WasmCloudApplication{}).
 		Owns(&corev1beta1.WasmCloudApplication{}).
 		Complete(r)
+}
+
+func (r *WasmCloudApplicationReconciler) deleteExternalResources(app *corev1beta1.WasmCloudApplication) error {
+	_, err := (&request.Sender{
+		Log: r.Log,
+	}).Send(request.Message{
+		// FIXME: make this Delete() and make send be Put()
+		Name:        app.Name,
+		Namespace:   app.Namespace,
+		Application: nil,
+	})
+	return err
+}
+
+// Helper functions to check and remove string from a slice of strings.
+func containsString(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func removeString(slice []string, s string) (result []string) {
+	for _, item := range slice {
+		if item == s {
+			continue
+		}
+		result = append(result, item)
+	}
+	return
 }
